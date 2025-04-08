d74429238f9bd53ee35febbe7f65e93a8ddd30b9
drdavis65 <92239386+drdavis65@users.noreply.github.com>
2025-04-08 11:52:55 -0700

    Create #303_range_sum_query.cpp

---
+++ prefix_sum/#303_range_sum_query.cpp
@@ -0,0 +1,22 @@
+class NumArray {
+public:
+    vector<int> sumArray;
+    NumArray(vector<int>& nums) {
+        if(nums.size() == 0) return;
+
+        int n = nums.size();
+
+        sumArray.resize(n);
+
+        sumArray[0] = nums[0];
+        for(int i = 1; i < n; i++) {
+            sumArray[i] += sumArray[i-1] + nums[i];
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        if(left == 0) return sumArray[right];
+
+        return sumArray[right] - sumArray[left-1];
+    }
+};
