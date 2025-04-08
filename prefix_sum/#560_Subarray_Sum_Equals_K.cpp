9cac723fd8d1763e0d841b6f25999595c356d5b2
drdavis65 <92239386+drdavis65@users.noreply.github.com>
2025-04-08 13:56:34 -0700

    Create #560_Subarray_Sum_Equals_K.cpp

---
+++ prefix_sum/#560_Subarray_Sum_Equals_K.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        int count = 0;
+        int sum = 0;
+        map[0] = 1;
+        for(int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if(map.find(sum - k) != map.end()) {
+                count += map[sum - k];
+            }
+            if(map.find(sum) != map.end()) {
+                map[sum] += 1;
+            }else {
+                map[sum] = 1;
+            }
+        }
+        return count;
+    }
+};
