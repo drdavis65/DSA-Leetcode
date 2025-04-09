24187dc64ab91a57f62a1aa59f2c09d4a54e4209
drdavis65 <92239386+drdavis65@users.noreply.github.com>
2025-04-09 08:50:17 -0700

    Update min_heap.cpp

--- Data_Structures/min_heap.cpp
+++ Data_Structures/min_heap.cpp
@@ -1,13 +1,23 @@
+#include <stdexcept>
+#include <iostream>
 
 template<typename T>
 class min_heap {
+private:
+  T* heap;
+  int capacity;
+  int size;
 
 public: 
-
-
-
-private:
-
+  min_heap() {
+    capacity = 32;
+    heap = new T[capacity];
+    size = 0;
+  }
+  
+  ~min_heap() { 
+    delete[] heap;
+  }
 
 
 };
