7106511ae9a49c59bb2dce64e086e90538ff10cc
drdavis65 <92239386+drdavis65@users.noreply.github.com>
2025-04-09 08:27:57 -0700

    Create #70_Climbing_Stairs.cpp

---
+++ DP/#70_Climbing_Stairs.cpp
@@ -0,0 +1,16 @@
+// Date last reviewed
+// 4/9/2025
+// =======================================================
+class Solution {
+public:
+    int climbStairs(int n) {
+        int steps[4];
+        steps[1] = 1;
+        steps[2] = 2;
+        for(int i = 3; i < n+1; i++) {
+            steps[i&3] = steps[(i-1)&3] + steps[(i-2)&3];
+        }
+        return steps[n&3];
+    }
+    
+};
