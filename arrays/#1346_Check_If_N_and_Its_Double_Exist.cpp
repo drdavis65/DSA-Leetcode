ef036777b0a9975b66b3443b7ae0f11cd28234e3
drdavis65 <92239386+drdavis65@users.noreply.github.com>
2025-04-15 16:29:43 -0700

    Create #1346_Check_If_N_and_Its_Double_Exist.cpp

---
+++ arrays/#1346_Check_If_N_and_Its_Double_Exist.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        std::unordered_map<int, int> seen;
+        for(int i = 0; i < arr.size(); i++) {
+            if(seen.find(arr[i]*2) != seen.end() ||
+              (arr[i] % 2 == 0) && seen.find(arr[i] / 2) != seen.end()) {
+                return true;
+            }
+            seen[arr[i]] = i;
+        }
+        return false;
+    }
+};
