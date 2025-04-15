6e6930cb964a8add156c7a10881be5fa55a8cb04
drdavis65 <92239386+drdavis65@users.noreply.github.com>
2025-04-14 18:43:05 -0700

    Create #977_Squares_of_a_Sorted_Array.cpp

---
+++ arrays/#977_Squares_of_a_Sorted_Array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> result;
+        int i;
+        for(i = 0; i < nums.size(); i++) {
+            if(nums[i] >= 0) break;
+        }
+        int j = i;
+        int k = i - 1;
+        while(j < nums.size() && k >= 0) {
+            if(std::abs(nums[k]) < nums[j]) {
+                result.push_back(nums[k]*nums[k]);
+                k--;
+            }else {
+                result.push_back(nums[j]*nums[j]);
+                j++;
+            }
+        }
+        if(j >= nums.size()) {
+            while(k >= 0) {
+                result.push_back(nums[k]*nums[k]);
+                k--;
+            }
+        }else {
+            while(j < nums.size()) {
+                result.push_back(nums[j]*nums[j]);
+                j++;
+            }
+        }
+        return result;
+    }
+};
