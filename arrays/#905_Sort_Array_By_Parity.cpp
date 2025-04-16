b46d07398cea637a87b6335c90ea5bcabea55e8a
drdavis65 <92239386+drdavis65@users.noreply.github.com>
2025-04-15 20:13:49 -0700

    Create #905_Sort_Array_By_Parity.cpp

---
+++ arrays/#905_Sort_Array_By_Parity.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int oddPtr = 0;
+        int evenPtr = nums.size() - 1;
+        while(oddPtr < evenPtr) {
+            if(nums[oddPtr] % 2 > nums[evenPtr] % 2) { 
+                std::swap(nums[oddPtr], nums[evenPtr]); 
+            }
+
+            if(nums[oddPtr] % 2 == 0) oddPtr++;
+            if(nums[evenPtr] % 2 == 1) evenPtr--;
+        }
+        return nums;
+    }
+};
