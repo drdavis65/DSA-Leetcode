be7872b0feae31edc7dfd8e0f75ff9d104832e32
drdavis65 <92239386+drdavis65@users.noreply.github.com>
2025-04-16 10:18:46 -0700

    Create 4#48_Find_All_Numbers_Disappeared_in_an_Array.cpp

---
+++ arrays/4#48_Find_All_Numbers_Disappeared_in_an_Array.cpp
@@ -0,0 +1,19 @@
+// Date last reviewed:
+// 4/16/2025
+// ==================================================
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++) {
+            int idx = std::abs(nums[i]) - 1;
+            if(nums[idx] > 0) {
+                nums[idx] *= -1;
+            } 
+        }
+        vector<int> result;
+        for(int j = 0; j < nums.size(); j++) {
+            if(nums[j] > 0) result.push_back(j+1);
+        }
+        return result;
+    }
+};
