34aaf1da1a5967443da84ed8b0babd04fbb80498
drdavis65 <92239386+drdavis65@users.noreply.github.com>
2025-04-15 19:43:48 -0700

    Create #283_Move_Zeroes.cpp

---
+++ arrays/#283_Move_Zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int wr_ptr = 0;
+        int zero_count = 0;
+        
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == 0) {
+                zero_count++;
+            }else{
+                nums[wr_ptr++] = nums[i];
+            }
+        }
+        
+        for(int j = n - zero_count; j < n; j++) {
+            nums[j] = 0;
+        }
+        
+    }
+};
