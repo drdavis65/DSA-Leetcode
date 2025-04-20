851a2da1692c9edba4507feb979b88c938927288
drdavis65 <92239386+drdavis65@users.noreply.github.com>
2025-04-19 18:30:27 -0700

    Update #94_Binary_Tree_Inorder_Traversal.cpp

--- trees/#94_Binary_Tree_Inorder_Traversal.cpp
+++ trees/#94_Binary_Tree_Inorder_Traversal.cpp
@@ -1,6 +1,17 @@
 // Date last reviewed:
 // 4/19/2025
 // ==================================================
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
@@ -23,3 +34,23 @@ public:
         return result;
     }
 };
+
+// RECURSIVE SOLUTION
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         std::vector<int> result;
+  
+//         helper(root, result);      
+        
+//         return result;
+//     }
+
+//     void helper(TreeNode* root, vector<int> &result) {
+//         if(!root) return;
+
+//         helper(root->left, result);
+//         result.push_back(root->val);
+//         helper(root->right, result);
+//     }
+// };
