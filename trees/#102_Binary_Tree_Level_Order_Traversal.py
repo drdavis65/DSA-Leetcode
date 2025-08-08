ac7b3d24abb35efe3710df61183dad2174ee65de
Dalton Davis <92239386+drdavis65@users.noreply.github.com>
2025-08-08 12:27:30 -0700

    Update #102_Binary_Tree_Level_Order_Traversal.py

--- trees/#102_Binary_Tree_Level_Order_Traversal.py
+++ trees/#102_Binary_Tree_Level_Order_Traversal.py
@@ -4,33 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        levels = []
-        if not root:
-            return levels
-        
-        level = 0
-        queue = deque(
-            [
-                root,
-            ]
-        )
-        while queue:
-            levels.append([])
+        q = deque()
+        if root:
+            q.append(root)
+        res = []
+        while(len(q) > 0):
+            level_len = len(q)
+            level = []
+            for i in range(level_len):
+                curr = q.popleft()
+                level.append(curr.val)
+                print(f"{curr.val} ", end="")
+                if curr.left:
+                    q.append(curr.left)
+                if curr.right:
+                    q.append(curr.right)
+            res.append(level)
+            print()
+        return res
 
-            level_length = len(queue)
-
-            for _ in range(level_length): # ensure popping only nodes in level
-                node = queue.popleft() # remove from queue
-
-                levels[level].append(node.val) # append to list within levels
-
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-
-            level += 1 # increase level
-        
-        return levels
