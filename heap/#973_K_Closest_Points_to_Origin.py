7c0bd818a1e83badeba3522fd08685fbe2a300a4
Dalton Davis <92239386+drdavis65@users.noreply.github.com>
2025-08-12 12:26:44 -0700

    Update #973_K_Closest_Points_to_Origin.py

--- heap/#973_K_Closest_Points_to_Origin.py
+++ heap/#973_K_Closest_Points_to_Origin.py
@@ -1,3 +1,5 @@
+# O(k*logn)
+# min heap
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         def getDistance(point):
@@ -8,4 +10,20 @@ class Solution:
         for _ in range(k):
             res.append(heapq.heappop(heap)[1])
         return res
-# can be further optimized with max heap or quick select
+# O(n*logk)
+# max heap
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        def getDistance(point):
+            return math.sqrt(point[0] ** 2 + point[1] ** 2)
+        heap = []
+        for point in points:
+            heapq.heappush(heap, (-getDistance(point), point))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        res = []
+        for _ in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
+
+# can be further optimized quick select
