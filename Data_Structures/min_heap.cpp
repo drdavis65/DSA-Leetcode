1aa3882021de6974206d400d92938674c2782025
Dalton Davis <92239386+drdavis65@users.noreply.github.com>
2025-08-12 08:12:32 -0700

    Update min_heap.cpp

--- Data_Structures/min_heap.cpp
+++ Data_Structures/min_heap.cpp
@@ -1,22 +1,66 @@
 #include <stdexcept>
 #include <iostream>
 
-template<typename T>
-class min_heap {
+class min_heap 
+{
 private:
-  T* heap;
-  int capacity;
-  int size;
-
+  vector<int> heap;
 public: 
-  min_heap() {
-    capacity = 32;
-    heap = new T[capacity];
-    size = 0;
+  min_heap() 
+  {
+    heap.push_back(0);
   }
   
-  ~min_heap() { 
-    delete[] heap;
+  void push(int val) 
+  {
+    heap.push_back(val);
+    int i = heap.size() - 1;
+
+    while(heap[i] < heap[i >> 1]) 
+    {
+      int temp = heap[i];
+      heap[i] = heap[i >> 1];
+      heap[i >> 1] = temp;
+      i = i >> 1;
+    }
+  }
+
+  int pop() 
+  {
+    if(heap.size() == 1) 
+    {
+      return -1;
+    }
+    if(heap.size() == 2) 
+    {
+      return heap.pop();
+    }
+    int ret = heap[1];
+    heap[1] = heap.pop();
+    i = 1;
+    while(2 * i < heap.size()) 
+    {
+      if(2 * i + 1 < heap.size() && 
+        heap[2*i + 1] < heap[2*i] && 
+        heap[i] > heap[2*i + 1]) 
+      {
+        int temp = heap[i];
+        heap[i] = heap[2*i+1];
+        heap[2*i+1] = temp;
+        i = i * 2 + 1;
+      }
+      else if(heap[i] > heap[2*i])
+      {
+        heap[i] = heap[2*i];
+        heap[2*i] = temp;
+        i = i * 2;
+      }
+      else 
+      {
+        break;
+      }
+    }
+    return ret;
   }
 
 
