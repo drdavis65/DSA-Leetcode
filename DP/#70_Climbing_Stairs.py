ee83b56fa392ee1040ca328fac245428200b55ed
Dalton Davis <92239386+drdavis65@users.noreply.github.com>
2025-10-26 14:55:28 -0700

    Update #70_Climbing_Stairs.py

--- DP/#70_Climbing_Stairs.py
+++ DP/#70_Climbing_Stairs.py
@@ -6,3 +6,12 @@ class Solution:
         for i in range(2, n):
             ways[i&1] = ways[0] + ways[1]
         return ways[(n-1)&1]
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n < 3:
+            return n
+        dp = [1,2]
+        for i in range(2,n+1):
+            dp[i%2] = dp[0] + dp[1]
+        return dp[(n+1)%2]
