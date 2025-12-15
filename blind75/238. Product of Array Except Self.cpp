ddfd019dd9c01a40e5c3cf2ce1ffc1cd730b2998
Dalton Davis <92239386+drdavis65@users.noreply.github.com>
2025-12-14 18:57:49 -0800

    Update 238. Product of Array Except Self.cpp

--- blind75/238. Product of Array Except Self.cpp
+++ blind75/238. Product of Array Except Self.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        std::vector<int> pref(n);
-        std::vector<int> suff(n);
         std::vector<int> result(n);
-        pref[0] = suff[n-1] = 1;
-        for (auto i{1}; i < n; ++i) {
-            pref[i] = pref[i-1] * nums[i-1];
-        }
-        for (auto i{n-2}; i >= 0; --i) {
-            suff[i] = suff[i+1] * nums[i+1];
-        }
+        int prefix = 1;
         for (auto i{0}; i < n; ++i) {
-            result[i] = pref[i] * suff[i];
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+        int postfix = 1;
+        for (auto i{n-1}; i >= 0; --i) {
+            result[i] *= postfix;
+            postfix *= nums[i];
         }
         return result;
     }
